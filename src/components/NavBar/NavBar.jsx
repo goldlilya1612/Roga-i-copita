0c8480d99261d34d76a562b4493c09a3c9779b3c
goldlilya1612 <goldlilya1612@yandex.com>
2021-05-03 02:50:08 +0300

    feat: сверстать всплывающее меню

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,10 +5,10 @@ import './NavBar.css'
 function NavBar() {
     return (
         <nav className="menu">
-            <NavLink exact to="/" className="menu__link">Список товаров</NavLink>
-            <NavLink to="/favorites" className="menu__link">Избранное</NavLink>
-            <NavLink to="/aboutUs" className="menu__link">О нас</NavLink>
-            <NavLink to="/personalAccount" className="menu__link menu__link_personal-account">Личный кабинет</NavLink>
+            <NavLink exact to="/" activeClassName="menu__link_active" className="menu__link">Список товаров</NavLink>
+            <NavLink to="/favorites" activeClassName="menu__link_active" className="menu__link">Избранное</NavLink>
+            <NavLink to="/aboutUs" activeClassName="menu__link_active" className="menu__link">О нас</NavLink>
+            <NavLink to="/personalAccount" activeClassName="menu__link_active" className="menu__link menu__link_personal-account">Личный кабинет</NavLink>
         </nav>
     )
 }
