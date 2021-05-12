2086805976bf846d54283dd5def6048c0fe8867f
Денис Марьянский <d.maryanskiy@gmail.com>
2021-05-12 22:59:24 +0300

    Pass data from api to react

--- HaH/HaH/settings.py
+++ HaH/HaH/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api',
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'HaH.urls'
@@ -135,3 +137,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- HaH/frontend/src/components/Product/Product.jsx
+++ HaH/frontend/src/components/Product/Product.jsx
@@ -1,83 +1,47 @@
 import React from 'react';
+import axios from 'axios';
 import './Product.css';
 import help from '../../images/products/1.jpg'
 import favourite from '../../images/favourite.svg'
 
-const Product = () => {
-    return (
-        <section className="elements"> 
-            <section className="element">
-                <img className="element__image" src={help}></img>
-                <h3 className="element__name">Виноград</h3>
-                <p className="element__category">Ягоды</p>
-                <p className="element__price">500 &#8381;</p>
-                <div className="element__buttons">
-                    <button className="element__button element__button_basket">добавить в корзину</button>
-                    <div className="element__button-wrapper">
-                        <img className="element__favourite" src={favourite}/>
-                        <button className="element__button_favourite"></button>
-                    </div>
-                </div>    
-            </section>
+class Product extends React.Component {
+    state = {
+        products:[]
+    }
 
-            <section className="element">
-                <img className="element__image" src={help}></img>
-                <h3 className="element__name">Виноград</h3>
-                <p className="element__category">Ягоды</p>
-                <p className="element__price">500 &#8381;</p>
-                <div className="element__buttons">
-                    <button className="element__button element__button_basket">добавить в корзину</button>
-                    <div className="element__button-wrapper">
-                        <img className="element__favourite" src={favourite}/>
-                        <button className="element__button_favourite"></button>
-                    </div>
-                </div>    
-            </section>
+    componentDidMount(){
+        axios.get("http://127.0.0.1:8000")
+        .then(res =>{
+            this.setState({
+                products:res.data
+            })
+            console.log(res.data);
+        })
+    }
 
-            <section className="element">
-                <img className="element__image" src={help}></img>
-                <h3 className="element__name">Виноград</h3>
-                <p className="element__category">Ягоды</p>
-                <p className="element__price">500 &#8381;</p>
-                <div className="element__buttons">
-                    <button className="element__button element__button_basket">добавить в корзину</button>
-                    <div className="element__button-wrapper">
-                        <img className="element__favourite" src={favourite}/>
-                        <button className="element__button_favourite"></button>
-                    </div>
-                </div>    
-            </section>
+    render() {
+        return (
+            <section className="elements">
+                {this.state.products.map(product => ( 
+                <section className="element">
+                    <img className="element__image" src={product.image}></img>
+                    <h3 className="element__name">{product.title}</h3>
+                    <p className="element__category">{product.category}</p>
+                    <p className="element__price">{product.price} &#8381;</p>
+                    <div className="element__buttons">
+                        <button className="element__button element__button_basket">добавить в корзину</button>
+                        <div className="element__button-wrapper">
+                            <img className="element__favourite" src={favourite}/>
+                            <button className="element__button_favourite"></button>
+                        </div>
+                    </div>    
+                </section>
+                ))}
 
-            <section className="element">
-                <img className="element__image" src={help}></img>
-                <h3 className="element__name">Виноград</h3>
-                <p className="element__category">Ягоды</p>
-                <p className="element__price">500 &#8381;</p>
-                <div className="element__buttons">
-                    <button className="element__button element__button_basket">добавить в корзину</button>
-                    <div className="element__button-wrapper">
-                        <img className="element__favourite" src={favourite}/>
-                        <button className="element__button_favourite"></button>
-                    </div>
-                </div>    
+               
             </section>
-
-            <section className="element">
-                <img className="element__image" src={help}></img>
-                <h3 className="element__name">Виноград</h3>
-                <p className="element__category">Ягоды</p>
-                <p className="element__price">500 &#8381;</p>
-                <div className="element__buttons">
-                    <button className="element__button element__button_basket">добавить в корзину</button>
-                    <div className="element__button-wrapper">
-                        <img className="element__favourite" src={favourite}/>
-                        <button className="element__button_favourite"></button>
-                    </div>
-                </div>    
-            </section>
-
-        </section>
-    )
+        )
+    }
 }
 
 export default Product;
